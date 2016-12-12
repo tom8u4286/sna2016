baed3e03a3c458fef7e210a763106cb17e04d882
Tomtom <tom8u4286@gmail.com>
2016-12-12 13:42:14 +0800

    add post

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from myapp import views
+
 urlpatterns = [
 	url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
