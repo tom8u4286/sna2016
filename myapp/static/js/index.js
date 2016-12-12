62cb90a28238ed37e0aa14b0dca511d344b5996a
Tomtom <tom8u4286@gmail.com>
2016-12-12 18:18:41 +0800

    fb first test

--- myapp/static/js/index.js
+++ myapp/static/js/index.js
@@ -1,3 +1,21 @@
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '754318864731770',
+  xfbml      : true,
+  version    : 'v2.8'
+});
+FB.AppEvents.logPageView();
+};
+window.fbloaded();
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 var loginCheck = function() {
     FB.getLoginStatus(function(response) {
         console.log("getLoginStatus()");
