c2abc167035bb5ebc5a1da4034f0f624e915e3dc
Tomtom <tom8u4286@gmail.com>
2016-12-12 15:08:23 +0800

    csrf_token added

--- myapp/views.py
+++ myapp/views.py
@@ -16,6 +16,3 @@ def index(request):
 			new_post_text = django_form.data.get("text")
 			Post.objects.create(text = new_post_text,)
 			return HttpResponseRedirect("/")
-		else:
-			post_list = Post.objects.all()
-			return render(request, 'myapp/index.html',{'posts': post_list})
